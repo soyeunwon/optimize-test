16b374cad61a5f254ff229ed53c694d154918ab6
dnjstd <dnjstd@gmail.com>
2024-02-10 20:15:50 +0900

    Code Splitting, bundle-analyzer

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { Switch, Route } from "react-router-dom";
 import "./App.css";
-import ListPage from "./feature/blog/pages/ListPage";
-import ViewPage from "./feature/blog/pages/ViewPage/index";
+
+const ListPage = lazy(() => import("./feature/blog/pages/ListPage"));
+const ViewPage = lazy(() => import("./feature/blog/pages/ViewPage"));
 
 function App() {
   return (
     <div className="App">
-      <Switch>
-        <Route path="/" exact>
-          <h1>optimize test</h1>
-        </Route>
-        <Route path="/blog" component={ListPage} exact />
-        <Route path="/blog/view/:id" component={ViewPage} exact />
-      </Switch>
+      <Suspense fallback={<div>로딩 중...</div>}>
+        <Switch>
+          <Route path="/" exact>
+            <h1>optimize test</h1>
+          </Route>
+          <Route path="/blog" component={ListPage} exact />
+          <Route path="/blog/view/:id" component={ViewPage} exact />
+        </Switch>
+      </Suspense>
     </div>
   );
 }
